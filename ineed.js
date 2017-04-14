8d38ce739e2f6c9affc8d97cbe4a29857488c945
devedge <devedge@outlook.com>
2017-04-14 00:21:15 -0400

    Most core functionality added. Fancy header also included

--- ineed.js
+++ ineed.js
@@ -5,20 +5,20 @@ const ineed = require('ineed');
 
 let images = true;
 let hyperlinks = true;
-let scripts = false;
-let stylesheets = false;
-let title = false;
+let scripts = true;
+let stylesheets = true;
+let title = true;
 
-let texts = false;
-let jsCode = false;
-let cssCode = false;
-let comments = false;
+let texts = true;
+let jsCode = true;
+let cssCode = true;
+let comments = true;
 
 let searchstring = '';
 
 let url = process.argv[2];
 
-console.time('init');
+// console.time('init');
 
 // Collect everything at once. Unofficial timing tests and ineed's 
 // one-pass strategy means that there is no appreciable time 
@@ -37,44 +37,84 @@ ineed
 		.comments		// HTML comments
 		.from(url, (err, response, result) => {
 
+
+	// On a fatal error, log it and quit
+	if (err) {
+		console.log('Error: URL is probably not valid');
+		process.exit(1);
+	} 
+	
+	// If the status code is not normal, log it
 	if (response.statusCode !== 200) {
-		console.log(response.statusCode);
+		console.log('Error Status Code: ' + response.statusCode);
 	}
 	
+	
+	// 
 	if (images) {
-		output(result.images, 'src');
+		outputResult(result.images, 'src');
 	}
 	if (hyperlinks) {
-		output(result.hyperlinks, 'href');
+		outputResult(result.hyperlinks, 'href');
 	}
 	if (scripts) {
-		
+		outputResult(result.scripts);
 	}
 	if (stylesheets) {
-		
+		outputResult(result.stylesheets);
 	}
 	if (title) {
-		
+		if (searchstring) {
+			if (result.title.indexOf(searchstring) !== -1) {
+				console.log(result.title);
+			}
+		} else {
+			console.log(result.title);
+		}
 	}
 	if (texts) {
-		
+		outputResult(result.texts);
 	}
 	if (jsCode) {
-		
+		outputResult(result.jsCode);
 	}
 	if (cssCode) {
-		
+		outputResult(result.cssCode);
 	}
 	if (comments) {
-		
+		outputResult(result.comments);
 	}
-	
-	console.timeEnd('init');
 });
 
 
-function output(elementArray, selector) {
-	elementArray.forEach((item) => {
-		console.log(item[selector]);
-	});
+/**
+ * [outputResult description]
+ * @method outputResult
+ * @param  {[type]} elementArray [description]
+ * @param  {[type]} selector     [description]
+ * @return {[type]}              [description]
+ */
+function outputResult(elementArray, selector) {
+	if (selector) {
+		elementArray.forEach((item) => {
+			if (searchstring) {
+				if (item[selector].indexOf(searchstring) !== -1) {
+					console.log(item[selector]);
+				}
+			} else {
+				console.log(item[selector]);
+			}
+		});
+
+	} else {
+		elementArray.forEach((item) => {
+			if (searchstring) {
+				if (item.indexOf(searchstring) !== -1) {
+					console.log(item);
+				}
+			} else {
+				console.log(item);
+			}
+		});
+	}
 }
